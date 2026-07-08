0e97798a3d13b89c309a2fa3129694a6a7f1840e
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-07-09 02:28:07 +0900

    [#6] chore: member-adapter-out에 Testcontainer/Kotest 테스트 인프라 의존성 추가
    - DataJpaTest, ServiceConnection, kotest-extensions-spring 등 adapter-out 테스트에 필요한 의존성 등록

--- module-member/member-adapter-out/build.gradle.kts
+++ module-member/member-adapter-out/build.gradle.kts
@@ -7,4 +7,8 @@ dependencies {
     implementation(project(":shared"))
     implementation(project(":common-persistence"))
     implementation(libs.spring.boot.starter.data.jpa)
+
+    testImplementation(libs.bundles.testcontainers)
+    testImplementation(libs.kotest.extensions.spring)
+    testRuntimeOnly(libs.postgresql)
 }
