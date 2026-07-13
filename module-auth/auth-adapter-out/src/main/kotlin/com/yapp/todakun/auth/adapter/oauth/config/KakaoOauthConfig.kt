e85e871a81fb5692b511eaeb2f1d246051315042
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-07-13 19:05:26 +0900

    [#6] refactor: KakaoOauthConfig RestClient에 connect/read 타임아웃 설정 추가

--- module-auth/auth-adapter-out/src/main/kotlin/com/yapp/todakun/auth/adapter/oauth/config/KakaoOauthConfig.kt
+++ module-auth/auth-adapter-out/src/main/kotlin/com/yapp/todakun/auth/adapter/oauth/config/KakaoOauthConfig.kt
@@ -4,11 +4,22 @@ import com.yapp.todakun.auth.adapter.oauth.kakao.KakaoOauthProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestClient
+import java.time.Duration
 
 @Configuration
 @EnableConfigurationProperties(KakaoOauthProperties::class)
 class KakaoOauthConfig {
     @Bean
-    fun restClient(): RestClient = RestClient.create()
+    fun restClient(): RestClient =
+        RestClient.builder()
+            .requestFactory(createRequestFactory())
+            .build()
+
+    private fun createRequestFactory() =
+        SimpleClientHttpRequestFactory().apply {
+            setConnectTimeout(Duration.ofSeconds(3))
+            setReadTimeout(Duration.ofSeconds(5))
+        }
 }
