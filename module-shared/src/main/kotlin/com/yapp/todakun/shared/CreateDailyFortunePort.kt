d3b528660d1021b764f422ec968e0d038a81c971
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-07-29 00:00:05 +0900

    [#32] docs: CreateDailyFortunePort KDoc에 일일 스케줄러 호출자 추가

--- module-shared/src/main/kotlin/com/yapp/todakun/shared/CreateDailyFortunePort.kt
+++ module-shared/src/main/kotlin/com/yapp/todakun/shared/CreateDailyFortunePort.kt
@@ -5,8 +5,8 @@ import java.util.UUID
 
 /**
  * 오늘의 운세 생성 크로스 도메인 포트.
- * - 온보딩(회원가입): auth가 신규 회원의 당일 운세를 생성한다.
- * - 회원가입 트랜잭션 안에서 호출되어 원자성을 보장한다.
+ * - 온보딩(회원가입): auth가 신규 회원의 당일 운세를 생성한다. 회원가입 트랜잭션 안에서 호출되어 원자성을 보장한다.
+ * - 일일 스케줄러: daily-fortune이 전체 회원의 당일 운세를 매일 미리 생성한다.
  */
 interface CreateDailyFortunePort {
     fun create(
