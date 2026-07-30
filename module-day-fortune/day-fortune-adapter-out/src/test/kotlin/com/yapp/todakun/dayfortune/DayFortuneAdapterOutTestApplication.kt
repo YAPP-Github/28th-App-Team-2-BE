d5bfeb4442a5fa1d3a1207dd2c3572ccb7e926ad
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-07-30 16:50:10 +0900

    [#37] test: DaySelectionFortuneRepositoryAdapterTest, VertexAiDaySelectionFortuneAdapterTest 단위 테스트 추가
    - DaySelectionFortuneFixture 추가

---
+++ module-day-fortune/day-fortune-adapter-out/src/test/kotlin/com/yapp/todakun/dayfortune/DayFortuneAdapterOutTestApplication.kt
@@ -0,0 +1,8 @@
+package com.yapp.todakun.dayfortune
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+@EnableJpaAuditing
+class DayFortuneAdapterOutTestApplication
