3e462fccab11ef6958e3ce41c23f79a8dacc44f8
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-08-16 00:46:27 +0900

    [#77] test: chat-adapter-out 영속성/Redis 어댑터 단위 테스트 추가
    - ChatConversationRepositoryAdapterTest, ChatMessageRepositoryAdapterTest 단위 테스트 추가
    - RedisChatQuotaAdapterTest 단위 테스트 추가
    - JPA 감사(auditing)는 별도 JpaAuditingTestConfig로 분리해 JPA 테스트에만 주입
    - @DataRedisTest 컨텍스트 로딩과 충돌 방지
    - application.yaml에서 테스트용 ddl-auto 설정 추가(create-drop)

--- module-chat/chat-adapter-out/src/test/kotlin/com/yapp/todakun/chat/ChatAdapterOutTestApplication.kt
+++ module-chat/chat-adapter-out/src/test/kotlin/com/yapp/todakun/chat/ChatAdapterOutTestApplication.kt
@@ -1,8 +1,6 @@
 package com.yapp.todakun.chat
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@EnableJpaAuditing
 class ChatAdapterOutTestApplication
