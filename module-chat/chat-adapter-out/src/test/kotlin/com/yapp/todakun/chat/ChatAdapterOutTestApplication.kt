1709b7a5cbbed33ed9b1226f5ea3476ce06193e3
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-08-16 00:36:17 +0900

    [#77] test: chat 도메인 테스트 인프라 구성
    - chat-adapter-out에 Testcontainers(Postgres/Redis) 기반 테스트 인프라 추가
    - chat-application/chat-adapter-out의 ChatFixture 추가

---
+++ module-chat/chat-adapter-out/src/test/kotlin/com/yapp/todakun/chat/ChatAdapterOutTestApplication.kt
@@ -0,0 +1,8 @@
+package com.yapp.todakun.chat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+@EnableJpaAuditing
+class ChatAdapterOutTestApplication
